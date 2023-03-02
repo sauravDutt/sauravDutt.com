75f447fcb9aebc77168c84855f24101dd30c5713
sauravDutt <sauravd069@gmail.com>
2023-03-02 18:55:16 +0530

    Just added some more stuff

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+let artIllustrations = [];
 fetch("art.json")
     .then(response => response.json())
-    .then(data => {
-        console.log(data);
-
+        .then(data => {
+            artIllustrations = data.Illustrations;
+            console.log(artIllustrations);
+            artIllustrations.forEach(el => {
+            console.log("This is the Title of Illustrations :  -  "+el.title);
+            console.log("This is the Description of Illustrations :  -  "+el.description);
+        });
     });
