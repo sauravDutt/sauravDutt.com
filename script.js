73278ebbba96733a5a6f4d948e4c9b7aae73a4a0
sauravDutt <sauravd069@gmail.com>
2023-03-13 20:54:23 +0530

    Just added some more stuff

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 let artIllustrations = [];
 fetch("art.json")
     .then(response => response.json())
-        .then(data => {
-            artIllustrations = data.Illustrations;
-            console.log(artIllustrations);
-            var dynamicCards= '';
-            artIllustrations.forEach(el => {
-                dynamicCards += `
+    .then(data => {
+        artIllustrations = data.Illustrations;
+        console.log(artIllustrations);
+        var dynamicCards= '';
+        artIllustrations.forEach(el => {
+            dynamicCards += `
                 <div>
                     <h1>${el.title}</h1>
                     <p class="card-info">
-                        ${el.description}  
+                        <span>"
+                            ${el.description}
+                        "</span>
+                        <br/>
+                        <i class="fa-brands fa-ethereum"></i> ${el.price}
                     </p>
                     <img src=${el.imageUrl} class="card-image"/>
                 </div>
-                `
+            `
         });
         document.getElementById('bannerContainer').innerHTML = dynamicCards;
     });
